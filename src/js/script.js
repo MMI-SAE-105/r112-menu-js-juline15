b18b3ee6f83281b69dc492d732d1450c920a39e3
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2024-11-13 11:40:07 +0100

    classe noscroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 // On cible les éléments à modifier
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector(".menu");
+const body = document.querySelector("body")
 
 // Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
 toggle.addEventListener("click", () => {
@@ -10,4 +11,5 @@ toggle.addEventListener("click", () => {
     // Mise à jour des attributs ARIA pour accessibilité
     nav.ariaHidden = isOpen;
     toggle.ariaExpanded = isClosed;
+    body.classList.toggle("noscroll", isClosed);
 });
